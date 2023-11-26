9fcd9c77c553117ae0f51a8c9aabdba6698bf6ef
Mikolaj Krawczyk <mkkrawczyk00@gmail.com>
2023-11-26 20:15:08 +0100

    Add structure of users list

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    name: 'Mikołaj Krawczyk',
+    average: '2.3',
+    attendance: '39%',
+  },
+  {
+    name: 'Joanna Wilczak',
+    average: '3.9',
+    attendance: '39%',
+  },
+  {
+    name: 'Kamil Polańczyk',
+    average: '4.3',
+    attendance: '72%',
+  },
+  {
+    name: 'Sławomir Martecki',
+    average: '5.1',
+    attendance: '100%',
+  },
+  {
+    name: 'Roman Romanowksi',
+    average: '1.9',
+  },
+];
---
+++ src/views/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UsersList from 'components/UsersList/UsersList';
+
+const Root = () => {
+  return (
+    <>
+      <UsersList></UsersList>
+    </>
+  );
+};
+
+export default Root;
